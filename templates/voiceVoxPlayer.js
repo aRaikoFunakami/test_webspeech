301659d6c236bf5e919114e95b4af392310101a1
Raiko Funakami <raiko.funakami@access-company.com>
2023-07-09 11:17:18 +0900

    fix abort bug

--- templates/voiceVoxPlayer.js
+++ templates/voiceVoxPlayer.js
@@ -2,8 +2,8 @@ class VoiceVoxPlayer {
 	constructor() {
 		this.audioPlayQueue = Promise.resolve();
 		this.audioGenerationQueue = [];
-		this.abortController = new AbortController(); ;
 		this.audioEndedHandler = null;
+		this.abortController = null;
 	}
 
 	playAudio(
@@ -12,18 +12,17 @@ class VoiceVoxPlayer {
 		audioQueryUrl = "http://127.0.0.1:50021/audio_query?speaker=1",
 		synthesisUrl = "http://127.0.0.1:50021/synthesis?speaker=1"
 	) {
-		this.abortController = new AbortController();
-
 		var audioQueryData = "&text=" + encodeURIComponent(textData);
 		audioQueryUrl = audioQueryUrl + audioQueryData;
 
+		let abortController = new AbortController();
 		let audioGeneration = fetch(audioQueryUrl, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/x-www-form-urlencoded"
 			},
 			body: "",
-			signal: this.abortController.signal
+			signal: abortController.signal
 		})
 			.then(response => response.json())
 			.then(data => {
@@ -35,63 +34,81 @@ class VoiceVoxPlayer {
 						"Content-Type": "application/json"
 					},
 					body: jsonData,
-					signal: this.abortController.signal
+					signal: abortController.signal
 				});
 			})
 			.then(response => response.blob())
 			.then(blob => {
 				var audio = new Audio();
 				audio.src = URL.createObjectURL(blob);
-				return audio;
+				return [audio, abortController];
 			})
 			.catch(error => {
-				console.error("音声生成エラー:", error);
+				console.error("voice generation error:", error);
+				throw error;
 			});
-		this.audioGenerationQueue.push([audioGeneration, textData, isStop]);
+		this.audioGenerationQueue.push([audioGeneration, textData, isStop, abortController]);
 		this.processAudioQueue();
 	}
 
 	processAudioQueue() {
 		this.audioPlayQueue = this.audioPlayQueue
 			.then(() => {
-				let [nextAudio, textData, isStop] = this.audioGenerationQueue.shift();
-				return nextAudio.then(audio =>
+				let nextTask = this.audioGenerationQueue.shift();
+				if (!nextTask) {
+					return Promise.resolve();
+				}
+				let [nextAudioPromise, textData, isStop, abortController] = nextTask;
+				this.abortController = abortController;
+
+				return nextAudioPromise.then(([audio]) =>
 					new Promise((resolve, reject) => {
 						const handleAbort = () => {
 							audio.pause();
 							audio.src = "";
-							audio.onended = null;
+							audio.removeEventListener("ended", handleEnded);
 							URL.revokeObjectURL(audio.src);
 							reject(new DOMException("Audio playback aborted", "AbortError"));
 						};
 
 						this.abortController.signal.addEventListener("abort", handleAbort);
 
-						audio.onended = () => {
+						const handleEnded = () => {
+							this.abortController.signal.removeEventListener("abort", handleAbort);
 							console.log(`audio.onended ${this.audioEndedHandler}, ${textData}`);
-							const e = {
-								text: textData,
-								isStop: isStop,
-							};
-							if (this.audioEndedHandler) {
-								this.audioEndedHandler(e); // イベントハンドラを呼び出す
+							if (this.audioGenerationQueue.length === 0) {
+								const e = {
+									text: textData
+								};
+								if (this.audioEndedHandler) {
+									this.audioEndedHandler(e);
+								}
 							}
 							resolve();
 						};
+
+						audio.addEventListener("ended", handleEnded);
+
 						audio.play().catch(reject);
 					})
 				);
 			})
 			.catch(error => {
-				console.error("再生エラー:", error);
+				console.error("playback error:", error);
+				throw error;
 			});
 	}
 
 	abort() {
 		if (this.abortController) {
 			this.abortController.abort();
-			this.abortController = null;
-			this.abortController = new AbortController(); 
 		}
+		for (let task of this.audioGenerationQueue) {
+			let [audioGeneration, textData, isStop, abortController] = task;
+			abortController.abort();
+		}
+		this.audioGenerationQueue = [];
+		this.audioPlayQueue = Promise.resolve();
+		this.abortController = null;
 	}
 }
